e89dfe8847b2fb62f3b8ade41f4e0b7509ef6c68
未希己 田中 <m.family.love@icloud.com>
2019-10-11 10:48:40 +0900

    タイトル

--- GameTemplate/Game/Title.h
+++ GameTemplate/Game/Title.h
@@ -7,12 +7,20 @@ public:
 
 	bool Start();
 	void Update();
+	
 
 	prefab::CSpriteRender* m_spritRender = nullptr;
 	prefab::CFontRender* m_fontRender = nullptr;
+	prefab::CFontRender* m_position = nullptr;
+	prefab::CFontRender* m_scale = nullptr;
+
 	CVector3 Color = { 1.0f, 1.0f, 1.0f };
 	int Colorjoutai = 0;
+
+	float scale = 5.0f;
+	int j_scale = 0;
 	
-	int timer = 0;
+	
+	
 };
 
